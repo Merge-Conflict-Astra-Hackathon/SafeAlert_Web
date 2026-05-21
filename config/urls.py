c6bc2e8ec3e2af06a2779aa31db71eb0aeb6032d
AvilioWatson <aviliowatson@gmail.com>
2026-05-22 02:58:26 +0700

    Fix static files serving in debug

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', lambda request: redirect('/dashboard/')),
@@ -16,5 +17,5 @@ urlpatterns = [
 
 # Serve static files during development and media uploads for demo deployments.
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
